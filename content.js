095ab227f7bc367e5a55ccd621c2ecc8418dc579
Ann Catherine Jose <ann.jose@gmail.com>
2024-12-14 14:16:31 -0800

    Cleaned up the main init code - removed redundant code from DOM loaded event handler. Now it is initialized only once.

--- content.js
+++ content.js
@@ -745,11 +745,8 @@ class HNEnhancer {
     }
 }
 
-document.hnEnhancer = new HNEnhancer();                       // Initialize immediately
-console.log('Initialized HN Enhancer');
-
-// Also initialize when DOM content is loaded (backup)
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM Content Loaded');
-    new HNEnhancer();
-});
+// Initialize the HNEnhancer. Note that we are loading this content script with the default run_at of 'document_idle'.
+// So this script is injected only after the DOM is loaded and all other scripts have finished executing.
+// This guarantees that the DOM of the main HN page is loaded by the time this script runs.
+document.hnEnhancer = new HNEnhancer();
+console.log('HN Enhancer initialized and ready');
