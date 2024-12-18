50c9ab2176810b9fabb601cc36d6d2b8baa54806
Ann Catherine Jose <ann.jose@gmail.com>
2024-12-18 14:23:57 -0800

    Add summarize comments feature to HN post page
    This PR adds the feature to summarize all comments in a post.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/levelup-apps/hn-enhancer?shareId=XXXX-XXXX-XXXX-XXXX).

--- content.js
+++ content.js
@@ -37,6 +37,7 @@ class HNEnhancer {
         document.head.prepend(otMeta);
 
         this.initSummarizationAI();
+        this.addSummarizeCommentsLink(); // Add the 'summarize comments' link
     }
 
     get isHomePage() {
@@ -1163,6 +1164,33 @@ class HNEnhancer {
                     break;
             }
         });
+    }
+
+    addSummarizeCommentsLink() {
+        const navLinks = document.querySelector('.subtext');
+        if (navLinks) {
+            const summarizeLink = document.createElement('a');
+            summarizeLink.href = '#';
+            summarizeLink.textContent = 'summarize comments';
+            summarizeLink.style.marginLeft = '10px';
+            summarizeLink.addEventListener('click', async (e) => {
+                e.preventDefault();
+                const itemId = this.getCurrentHNItemId();
+                if (itemId) {
+                    const thread = await this.getHNThread(itemId);
+                    if (this.summaryPanel.style.display === 'none') {
+                        this.toggleSummaryPanel();
+                    }
+                    this.summarizeTextWithAI(thread);
+                }
+            });
+            navLinks.appendChild(summarizeLink);
+        }
+    }
+
+    getCurrentHNItemId() {
+        const itemIdMatch = window.location.search.match(/id=(\d+)/);
+        return itemIdMatch ? itemIdMatch[1] : null;
     }
 }
 
