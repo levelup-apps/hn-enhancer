c23bcc19f5f9c29805c292b08d155f414da2f4fb
Ann Catherine Jose <ann.jose@gmail.com>
2024-12-18 14:52:00 -0800

    Render markdown summary in HN post page
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/levelup-apps/hn-enhancer?shareId=XXXX-XXXX-XXXX-XXXX).

--- content.js
+++ content.js
@@ -400,9 +400,23 @@ class HNEnhancer {
 
     updateSummaryText(text) {
         const summaryTextElement = this.summaryPanel.querySelector('.summary-text');
-        summaryTextElement.innerHTML = text;
-
+        summaryTextElement.innerHTML = this.convertMarkdownToHTML(text);
     }
+
+    convertMarkdownToHTML(markdown) {
+        const html = markdown
+            .replace(/^### (.*$)/gim, '<h3>$1</h3>')
+            .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+            .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+            .replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
+            .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
+            .replace(/\*(.*)\*/gim, '<i>$1</i>')
+            .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
+            .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
+            .replace(/\n$/gim, '<br />');
+        return html.trim();
+    }
+
     navigateNextChild() {
         if (!this.currentComment) return;
 
